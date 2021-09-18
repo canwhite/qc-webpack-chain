476a6956987f1f85503ac6b2620a28865cab4efd
qiaochao <752716886@qq.com>
2021-09-18 17:34:44 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 
 const {merge} = require('webpack-merge')
-const config = require("./webpack-chain")
+const config = require("./webpack-base-chain")
 
 //merge可以合并base配置，做特定配置操作
 const webpackConfig = merge(config,{
